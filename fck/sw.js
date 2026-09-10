b67550a213a2ec824143c60993ed9005b27e0df2
Dennis Werner <dennis.werner.88@gmail.com>
2026-09-10 21:14:49 +0200

    feat: Wochentage bei Terminen anzeigen
    Die Spieltagsübersicht zeigte nur Anstoßzeiten, obwohl sich ein Spieltag
    über Freitag bis Sonntag verteilt. Die Partien werden jetzt nach Tagen
    gruppiert, mit dem ausgeschriebenen Wochentag als Zwischenüberschrift.
    
    Blätterelement und Untertitel nennen den Wochentag ebenfalls; damit
    erscheint er auch im erzeugten Bild.
    
    Version 1.4.1.

--- fck/sw.js
+++ fck/sw.js
@@ -12,7 +12,7 @@
  *
  * VERSION bei jeder Änderung an ausgelieferten Dateien hochzählen.
  */
-const VERSION = 'v1.4.0';
+const VERSION = 'v1.4.1';
 const APP   = 'app-'   + VERSION;
 const DATEN = 'daten-' + VERSION;
 
